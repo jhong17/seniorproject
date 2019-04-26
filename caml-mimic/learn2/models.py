fixed the metrics errors, removed softmax

caml-mimic/learn2/models.py
@@ -424,7 +424,8 @@ class AttentionWordRNN(nn.Module):
         word_squish = batch_matmul_bias(output_word, self.weight_W_word,self.bias_word, nonlinearity='tanh')
         word_attn = batch_matmul(word_squish, self.weight_proj_word)
         word_attn_norm = self.softmax_word(word_attn.transpose(1,0))
-        word_attn_vectors = attention_mul(output_word, word_attn_norm.transpose(1,0))        
+        #word_attn_vectors = attention_mul(output_word, word_attn_norm.transpose(1,0))        
+        word_attn_vectors = attention_mul(output_word, word_attn)        
         return word_attn_vectors.unsqueeze(0), state_word, word_attn_norm
     
     def init_hidden(self):
@@ -469,14 +470,17 @@ class AttentionSentRNN(nn.Module):
         
         
     def forward(self, word_attention_vectors, state_sent):
+        #import pdb; pdb.set_trace()
         output_sent, state_sent = self.sent_gru(word_attention_vectors, state_sent)        
         sent_squish = batch_matmul_bias(output_sent, self.weight_W_sent,self.bias_sent, nonlinearity='tanh')
         sent_attn = batch_matmul(sent_squish, self.weight_proj_sent)
         sent_attn_norm = self.softmax_sent(sent_attn.transpose(1,0))
-        sent_attn_vectors = attention_mul(output_sent, sent_attn_norm.transpose(1,0))        
+        #sent_attn_vectors = attention_mul(output_sent, sent_attn_norm.transpose(1,0))        
+        sent_attn_vectors = attention_mul(output_sent, sent_attn)        
         # final classifier
         final_map = self.final_linear(sent_attn_vectors.squeeze(0))
-        return F.log_softmax(final_map), state_sent, sent_attn_norm
+        #return F.log_softmax(final_map), state_sent, sent_attn_norm
+        return final_map, state_sent, sent_attn_norm
 
 
     def init_hidden(self):

caml-mimic/learn2/training.py
@@ -260,8 +260,13 @@ def test(model, Y, epoch, data_path, fold, gpu, version, code_inds, dicts, sampl
         get_attn = samples and (np.random.rand() < 0.02 or (fold == 'test' and testing))
         output, loss, alpha = model(data, target, desc_data=desc_data, get_attention=get_attn)
 
-        output = F.sigmoid(output)
+        #import pdb;pdb.set_trace()
         output = output.data.cpu().numpy()
+        #output_norm = output / np.linalg.norm(output)
+        output_sig = 1. / (1. + np.exp(-1.*output))
+
+        #output = F.sigmoid(output)
+        #output = output.data.cpu().numpy()
         losses.append(loss.data)
         target_data = target.data.cpu().numpy()
         if get_attn and samples:
@@ -284,6 +289,7 @@ def test(model, Y, epoch, data_path, fold, gpu, version, code_inds, dicts, sampl
     yhat_raw = np.concatenate(yhat_raw, axis=0)
 
     #write the predictions
+    import pdb;pdb.set_trace()
     preds_file = persistence.write_preds(yhat, model_dir, hids, fold, ind2c, yhat_raw)
     #get metrics
     k = 5 if num_labels == 50 else [8,15]